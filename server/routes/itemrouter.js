524db052170764f9592505b1875f0212f2ed6714
Tre Olson <treolson@live.com>
2021-09-01 16:55:27 -0500

    further setup

---
+++ server/routes/itemrouter.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const axios = require("axios");
+
+let config = {
+  //authenticate Big Commerce API
+  headers: {
+    "X-Auth-Client": process.env.BG_AUTH_CLIENT,
+    "X-Auth-Token": process.env.BG_AUTH_TOKEN,
+  },
+};
+
+let dateNow = moment().subtract(1, "days").format('YYYY-MM-DD')
+let dateThen = moment().subtract(31, "days").format("YYYY-MM-DD");
+
+setInterval(() => {
+  dateNow = moment().format("YYYY-MM-DD");
+  dateThen = moment().subtract(30, "days").format("YYYY-MM-DD");
+}, 1000 * 60 * 60 * 24);
+
+app.get("/items", (req, res) => {
+  axios
+    .get(
+      `https://api.bigcommerce.com/stores/et4qthkygq/v3/catalog/products?inventory_level=0`,
+      config
+    )
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+});
+
+let daterange = moment().subtract(6, "hours").subtract(1, "years");
+
+const {
+  //login verification middleware
+    rejectUnauthenticated,
+  } = require("../modules/authentication-middleware");
+
+router.get("/itemlist", rejectUnauthenticated, (req, res) => {
+  console.log("We are about to get the item list");
+
+  const queryText = `select * from "item" ORDER BY id DESC`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error on item query ${error}`);
+      res.sendStatus(500);
+    });
+})
+
+router.post("/orderdetails", rejectUnauthenticated, (req, res) => {
+  let order_number = req.body.order_number;
+  console.log("this is the payload before it reaches the get", order_number);
+  axios
+    .get(
+      `https://api.bigcommerce.com/stores/et4qthkygq/v2/orders/${order_number}/products`,
+      config
+    )
+    .then(function (response) {
+      console.log("this is the response", response.data)
+
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,66 +9,35 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const moment = require("moment");
 const express = require("express");
 const bodyParser = require("body-parser");
-const pool = require("./pool");
+const pool = require("./modules/pool");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("build"));
 
+const passport = require('./strategies/user.strategy');
+
+// Route includes
+const userRouter = require('./routes/userrouter');
+const itemRouter = require('./routes/itemrouter');
+
 //change this to push update 1
 
-let dateNow = moment().subtract(1, "days").format('YYYY-MM-DD')
-let dateThen = moment().subtract(31, "days").format("YYYY-MM-DD");
+app.use(sessionMiddleware);
 
-setInterval(() => {
-  dateNow = moment().format("YYYY-MM-DD");
-  dateThen = moment().subtract(30, "days").format("YYYY-MM-DD");
-}, 1000 * 60 * 60 * 24);
+// start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
 
-let config = {
-  headers: {
-    "X-Auth-Client": process.env.BG_AUTH_CLIENT,
-    "X-Auth-Token": process.env.BG_AUTH_TOKEN,
-  },
-};
-
-app.get("/items", (req, res) => {
-  axios
-    .get(
-      `https://api.bigcommerce.com/stores/et4qthkygq/v3/catalog/products?inventory_level=0`,
-      config
-    )
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    });
-});
-
-let daterange = moment().subtract(6, "hours").subtract(1, "years");
-
-app.delete("/deleteitemrange", (req, res) => {
-  pool
-    .query('DELETE FROM "item" WHERE created_at<=$1', [daterange])
-    .then((result) => {
-      res.sendStatus(204); //No Content
-    })
-    .catch((error) => {
-      console.log("Error DELETE ", error);
-      res.sendStatus(500);
-    });
-});
+app.use('/api/user', userRouter);
+app.use('/api/item', itemRouter);
   
 const token = process.env.SLACK_TOKEN;
 
 const web = new WebClient(token);
 
 const conversationId = "C0139RJPUEM";
-
-
 
 // Handle errors (see `errorCodes` export)
 slackEvents.on('error', console.error);
@@ -90,46 +59,6 @@ slackEvents.start().then(() => {
     })();
     console.log("bot listening on port", PORT);
 });
-
-
-
-app.get("/itemlist", (req, res) => {
-  console.log("We are about to get the item list");
-
-  const queryText = `select * from "item" ORDER BY id DESC`;
-  pool
-    .query(queryText)
-    .then((result) => {
-      res.send(result.rows);
-    })
-    .catch((error) => {
-      console.log(`Error on item query ${error}`);
-      res.sendStatus(500);
-    });
-})
-
-app.post("/orderdetails", (req, res) => {
-  let order_number = req.body.order_number;
-  console.log("this is the payload before it reaches the get", order_number);
-  axios
-    .get(
-      `https://api.bigcommerce.com/stores/et4qthkygq/v2/orders/${order_number}/products`,
-      config
-    )
-    .then(function (response) {
-      console.log("this is the response", response.data)  
-   
-        res.send(response.data);
-      })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-});
-
-setInterval(() => {
-  console.log('This is not setup yet');
-}, 1000 * 60 * 60 * 24);
      
 
  const PORT = process.env.PORT || 5000;
